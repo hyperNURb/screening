b67c9b2c164480541d04686bb1b1e800810ba4d1
Goltha <goltha@gmail.com>
2016-05-05 19:25:50 +0200

    Init commit

--- app/basics.js
+++ app/basics.js
@@ -8,7 +8,9 @@ exports.arrays = {
     output: [2, 4, 6]
     */
   square: function (arr) {
-
+    if (Array.isArray(arr)) {
+      return arr.map(function (a) { return a * a });
+    }
   },
   /*
     Sum of all array items
@@ -16,7 +18,11 @@ exports.arrays = {
     output: 6
     */
   sum: function (arr) {
-
+    if (Array.isArray(arr)) {
+      return arr.reduce(function (prev, curr) {
+        return prev + curr
+      });
+    }
   },
   /*
     Merges arrays
@@ -24,6 +30,10 @@ exports.arrays = {
     return: [1, 2, 3, 4, 5, 6, 7, 8, 9]
    */
   merge: function () {
+
+    return (Array.from(arguments)).reduce(function (prev, curr) {
+      return prev.concat(curr)
+    });
 
   },
   /*
@@ -33,12 +43,30 @@ exports.arrays = {
    */
   unique: function (arr) {
 
+    var returnArr = [];
+
+    if (Array.isArray(arr)) {
+
+      if (arr.length === 1) return arr;
+
+      return arr.reduce(function (prev, curr, idx, arr) {
+
+        if (idx === 1) {
+          returnArr.push(prev);
+        }
+        if (returnArr.indexOf(curr) === -1) {
+          returnArr.push(curr);
+        }
+        return returnArr;
+
+      });
+    }
   },
   /*
     Map
    */
   map: function (arr, callback) {
-
+    return arr.map(callback);
   }
 };
 
@@ -48,6 +76,14 @@ exports.objects = {
    */
   each: function (obj, callback) {
 
+    for (var prop in obj) {
+      if (obj.hasOwnProperty(prop)) {
+        obj[prop] = callback.apply(obj, [obj[prop]]);
+      }
+    }
+
+    return obj;
+
   },
   /*
     Reverse keys
@@ -56,6 +92,15 @@ exports.objects = {
    */
   reverseKeys: function (obj) {
 
+    var keys = [];
+
+    for (var prop in obj) {
+
+      if (obj.hasOwnProperty(prop)) {
+        keys.push(prop);
+      }
+    }
+    return keys.reverse();
   },
   /*
     Keys
@@ -63,13 +108,21 @@ exports.objects = {
     output: [1, 2, 3]
    */
   values: function (obj) {
+    var values = [];
+    for (var prop in obj) {
+      if (obj.hasOwnProperty(prop)) {
+        values.push(obj[prop]);
+      }
+    }
+    return values;
 
   },
   /*
     Extend
    */
   extend: function (obj1, obj2) {
-
+    obj1.prototype = obj2.prototype;
+    obj1.prototype.constructor = obj1;
   },
   /*
     Get
@@ -77,7 +130,12 @@ exports.objects = {
     output: 2
    */
   get: function (obj, prop) {
-
+    var fields = prop.split(".");
+    if (fields.length > 1) {
+      return this.get(obj[fields.shift()], fields.join("."));
+    } else {
+      return obj[fields];
+    }
   }
 };
 
@@ -91,14 +149,17 @@ exports.functions = {
     callit(add, 1, 2, 3, 4); // 10
    */
   callIt: function () {
-
+    var arraySlice = Array.prototype.slice;
+    var fn = arguments[0];
+    var storedArgs = arraySlice.call(arguments, 1);
+    return fn.apply(null, storedArgs);
   },
   /*
     Change function context
     NOTE: You are not allowed to use native fn.bind
    */
   bind: function (fn, context) {
-
+    return function () { fn.apply(context, Array.prototype.slice.call(arguments)) };
   },
   /*
     Partial function
@@ -110,6 +171,12 @@ exports.functions = {
    */
   partial: function (fn) {
 
+    var prevArgs = Array.prototype.slice.call(arguments, 1);
+    return function () {
+      var currArgs = Array.prototype.slice.call(arguments);
+      var args = prevArgs.concat(currArgs);
+      return fn.apply(null, args);
+    }
   }
 };
 
@@ -120,16 +187,58 @@ exports.regexes = {
     output: true
    */
   hasNumber: function (str) {
-
+    return /.*\d.*/.test(str);
   },
   /*
     Is IP address format
     input: '192.168.0.1'
     output: true
-
     NOTE: Simple check is enough
    */
   isIp: function (str) {
-
+    return /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/.test(str);
   }
 };
+
+console.log(exports.arrays.square([1, 2, 3]));
+console.log(exports.arrays.sum([1, 2, 3]));
+console.log(exports.arrays.merge([1, 2, 3], [4, 5, 6], [7, 8, 9]));
+console.log(exports.arrays.map([1, 2, 3], function (a) { return a * a }));
+console.log(exports.arrays.unique([1, 2, 3, 4]));
+console.log(exports.arrays.unique([1, 2, 1, 3, 4, 5, 2]));
+console.log(exports.objects.each({ 'a': 1, 'b': 2 }, Math.sqrt));
+console.log(exports.objects.reverseKeys({ a: 1, b: 2, c: 3 }));
+console.log(exports.objects.values({ a: 1, b: 2, c: 3 }));
+
+//Since the parameter in function is named obj and there are no quotes around 
+//first argument value, we will assume that object is given in the 
+//faulty notaion, and that correct one is {a: {b: {c: { d: 2}}}} 
+console.log(exports.objects.get({ a: { b: { c: { d: 2 } } } }, 'a.b.c.d'));
+
+function add() {
+
+  var sum = 0;
+
+  for (var i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+
+  return sum;
+
+}
+console.log(exports.functions.callIt(add, 1, 2, 3, 4, 5));
+
+
+
+function log() {
+
+  for (var i = 0; i < arguments.length; i++) {
+    console.log(arguments[i]);
+  }
+}
+exports.functions.partial(log, 'a', 'b')('c', 'd', 'e'); // a, b, c, d, e
+
+console.log(exports.regexes.hasNumber('abc2def'));
+console.log(exports.regexes.isIp('192.0.0.122'));
+
+
